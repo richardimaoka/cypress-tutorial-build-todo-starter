3c5df776a022abcbc469e7936ba0a9086f5faacd
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-29 21:16:02 +0900

    plural succeeds

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,8 +1,14 @@
 describe("Footer", () => {
   context("with a single todo", () => {
-    it.only("displays a singular todo in count", () => {
+    it("displays a singular todo in count", () => {
       cy.seedAndVisit([{ id: 1, name: "Buy milk", isComplete: false }]);
       cy.get(".todo-count").should("contain", "1 todo left");
+    });
+  });
+  context("with multiple todos", () => {
+    it.only("displays plural todos in count", () => {
+      cy.seedAndVisit();
+      cy.get(".todo-count").should("contain", "3 todos left");
     });
   });
 });

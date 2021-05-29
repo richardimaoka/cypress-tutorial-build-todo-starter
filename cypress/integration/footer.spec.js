2016575f7f71f35d9bc225940b7a2fe1a81dd323
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-29 21:47:14 +0900

    completed 7

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -5,17 +5,27 @@ describe("Footer", () => {
       cy.get(".todo-count").should("contain", "1 todo left");
     });
   });
+
   context("with multiple todos", () => {
     beforeEach(() => {
       cy.seedAndVisit();
     });
+
     it("displays plural todos in count", () => {
       cy.get(".todo-count").should("contain", "3 todos left");
     });
-    it.only("Filters to active todos", () => {
-      cy.contains("Active").click();
 
-      cy.get(".todo-list li").should("have.length", 3);
+    it("Handles filter links", () => {
+      const filters = [
+        { link: "Active", expectedLength: 3 },
+        { link: "Completed", expectedLength: 1 },
+        { link: "All", expectedLength: 4 },
+      ];
+      cy.wrap(filters).each((filter) => {
+        cy.contains(filter.link).click();
+
+        cy.get(".todo-list li").should("have.length", filter.expectedLength);
+      });
     });
   });
 });
